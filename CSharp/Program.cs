01681700c9609bb731c83a77f68c3d79964e6b17
lala9663 <lala96632040@gmail.com>
2024-09-01 15:47:16 +0900

    .gitattributes, .gitignore 및 LICENSE.txt을(를) 추가합니다.

---
+++ CSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hleeo, World!");
+        }
+    }
+}
