caee761a6d007ff18ed327943de441ae9e2ac272
lala9663 <lala96632040@gmail.com>
2024-09-03 00:17:53 +0900

    jetBrain Rider 연동

---
+++ CSharp/기초문법/Study.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharp;
+
+public class Study
+{
+    static void main(string[] args)
+    {
+        // int
+        // float
+        // string
+        // bool
+        
+        // isAdmin = true/false
+        // soundOnOff = true/false
+        // autoPlay = true/false
+        
+        
+    }
+}
