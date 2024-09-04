1258e50500ba626b393631932d99acc117de779e
lala9663 <lala96632040@gmail.com>
2024-09-04 23:42:46 +0900

    출력 방식 종류

--- CSharp/기초문법/Study.cs
+++ CSharp/기초문법/Study.cs
@@ -12,7 +12,25 @@ public class Study
         // isAdmin = true/false
         // soundOnOff = true/false
         // autoPlay = true/false
+
         
         
+        // "1"
+        // string input = Console.ReadLine();
+        //
+        // int number = int.Parse(input);
+        // Console.WriteLine(number);
+
+        int hp = 100;
+        int maxHp = 100;
+
+        string message = "당신의 HP는" + hp + "입니다";
+        Console.WriteLine(message);
+
+        string.Format("당신의 HP는 {0} {1} 입니다", hp, maxHp);
+        Console.WriteLine(message);
+        
+        string message1 = $"당신의 HP는 {hp} / {maxHp}입니다";
+        Console.WriteLine(message1);
     }
 }
